629074bcf5d4afccc1e7513519df288d3bfd3963
dangamtechno <138078538+dangamtechno@users.noreply.github.com>
2024-05-21 14:03:24 -0400

    Update user.php

--- user.php
+++ user.php
@@ -1,14 +1,14 @@
 <?php
-// Insert customer
-function insertUser($name, $email, $password, $address) {
-    $query = "INSERT INTO users (name, email, password, address) VALUES (?, ?, ?, ?)";
+// Insert user
+function insertUser($first_name, $last_name, $username, $email, $password, $phone = null) {
+    $query = "INSERT INTO users (first_name, last_name, username, email, password_hash, phone) VALUES (?, ?, ?, ?, ?, ?)";
     $stmt = $conn->prepare($query);
-    $stmt->bind_param("ssss", $name, $email, $password, $address);
+    $stmt->bind_param("ssssss", $first_name, $last_name, $username, $email, $password, $phone);
     $stmt->execute();
     return $conn->insert_id;
 }
 
-// Get customer by ID
+// Get user by ID
 function getUserById($user_id) {
     $query = "SELECT * FROM users WHERE user_id = ?";
     $stmt = $conn->prepare($query);
