f9535afaa9348f080cfa0da292358b1f35e4133a
dangamtechno <138078538+dangamtechno@users.noreply.github.com>
2024-05-28 16:25:51 -0400

    Update login.js

--- DataDash/frontend/js/login.js
+++ DataDash/frontend/js/login.js
@@ -3,23 +3,17 @@ const registerButton = document.querySelector('.register');
 const logoutButton = document.querySelector('.logout');
 const loggedUser = document.querySelector('.logged-user');
 
-// Add event listener to register button
-registerButton.addEventListener('click', () => {
-  showHideIcon(registerButton, true);
-});
-
 // Check if a session exists using AJAX
-fetch('check_session.php')
+fetch('get_session.php')
   .then(response => response.text())
   .then(data => {
     if (data === 'true') {
-      // Get the user ID using AJAX
-      fetch('get_user_id.php')
+      // Get the username using AJAX
+      fetch('get_username.php')
         .then(response => response.text())
-        .then(userId => {
-          loggedUser.textContent = `Logged in as ${userId}`;
-          loginButton.style.display = 'none'; // Hide only the login button
-          registerButton.style.display = 'none'; // Hide only the register button
+        .then(username => {
+          loggedUser.textContent = `Logged in as ${username}`;
+
         });
     }
   });
