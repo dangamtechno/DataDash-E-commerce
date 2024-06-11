56abf1b7a038022e1b45f042e10cd09885c2ff01
jmd246 <jdoucette69.jd@gmail.com>
2024-06-11 09:52:22 -0400

    added  past reviews section
    added  past reviews section and here are screen shots of it working.

--- DataDash/backend/utils/get_reviews.php
+++ DataDash/backend/utils/get_reviews.php
@@ -3,17 +3,20 @@ require '../include/database_config.php';
 header('Access-Control-Allow-Origin: *');
 
 if($_SERVER['REQUEST_METHOD']== "GET" && isset($_GET['id'])){
-    $stmt = "SELECT quantity FROM inventory where product_id = ?;";
+    $stmt = "SELECT reviews.*,users.fname,users.lname FROM reviews
+             Join ordered_item on reviews.order_id = ordered_item.order_id 
+             join users on reviews.user_id = users.id
+             WHERE ordered_item.product_id=?;";
     $prep_stmt = $conn->prepare($stmt);
     $id = $_GET['id'];
     $prep_stmt->bind_param('i',$id);
     $prep_stmt->execute();
     if($result = $prep_stmt->get_result()){
-        //$arr = array();
-        //while($rowArray= $result->fetch_assoc()){
-        //array_push($arr,$result->fetch_assoc());
-        //}
-        echo json_encode(['inStock' => $result->fetch_assoc()['quantity']]);
+        $arr = array();
+        while($rowArray= $result->fetch_assoc()){
+           array_push($arr,$rowArray);
+        }
+        echo json_encode(['reviews' => $arr]);
     }          
     else{
         echo json_encode(['error' => "Something went wrong"]);
