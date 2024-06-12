1099e5b1408a950334d6c523da5442f73f6c1353
dangamtechno <138078538+dangamtechno@users.noreply.github.com>
2024-06-12 14:27:36 -0400

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -114,8 +114,8 @@ CREATE TABLE returns (
 
 -- Shopping cart and wishlist tables
 CREATE TABLE cart (
+    cart_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
-    PRIMARY KEY (user_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
