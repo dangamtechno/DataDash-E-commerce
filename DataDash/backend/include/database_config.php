bae2028725452beadde41a4dc8520226079ffe73
jmd246 <jdoucette69.jd@gmail.com>
2024-06-17 17:00:53 -0400

    changed access control
    changed access control in database_config.php

--- DataDash/backend/include/database_config.php
+++ DataDash/backend/include/database_config.php
@@ -1,5 +1,6 @@
 <?php
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: http://127.0.0.1:5501');
+header('Access-Control-Allow-Credentials: true');
 
 session_start();
 
